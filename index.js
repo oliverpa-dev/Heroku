0d132eb3cbfd8c14c6ce02eaf4cccb9014c7871c
Oliver <oliver.paszkiewicz@interia.pl>
2021-06-06 18:28:06 +0200

    added time

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("WORKING!!!");
+    let time = new Date();
+    res.send(time);
 })
 
 app.listen(process.env.PORT || 5000)
