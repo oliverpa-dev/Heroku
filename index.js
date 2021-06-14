fb00a974e7e7407a859051bf9dc0e246fd510ebd
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2021-06-14 20:28:20 +0200

    added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const https = require("http");
 
-app.get("/", function(req, res) {
+// const options = {
+//     hostname: 'https://date-time-server.herokuapp.com/',
+//     port: 443,
+//     path: '/local',
+//     method: 'GET'
+// }
+
+app.get("/home1", function(req, res) {
     let time = new Date();
     let time_2 = time.toGMTString();
     res.send(time_2);
 })
+
+// app.get("/local")
 
 app.listen(process.env.PORT || 5000)
