374a7fcb55bc7b59579b25fd4365e15ae2ddd558
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2021-06-14 20:54:43 +0200

    Added local path

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
-const https = require("http");
 
-// const options = {
-//     hostname: 'https://date-time-server.herokuapp.com/',
-//     port: 443,
-//     path: '/local',
-//     method: 'GET'
-// }
-
-app.get("/home1", function(req, res) {
+app.get("/", function(req, res) {
     let time = new Date();
     let time_2 = time.toGMTString();
     res.send(time_2);
+})
+
+app.get("/local", function(req, res) {
+    let time_3 = Date()
+    let time_3GMT = time_3.toGMTString();
+    res.send(time_3GMT);
 })
 
 // app.get("/local")
